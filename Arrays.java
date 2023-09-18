f167dd50b06716dbdc68cc1a1ddf11235001c756
Brad <brad.nguyen@mrjaffes.class>
2023-09-17 19:28:36 -0700

    Finished Arrays

--- Arrays.java
+++ Arrays.java
@@ -2,6 +2,17 @@ public class Arrays
 {
     public static void main(String[] args)
     {
-        
+        String[] stringArrays = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};  
+        for (int i = 0; i < stringArrays.length; i++) {
+            System.out.println(stringArrays[i]);
+        }
+        for (int i = 0; i < stringArrays.length; i++) {
+            System.out.print(stringArrays[i]);
+        }
+        System.out.println(" ");
+        for (int i = 0; i < stringArrays.length; i++) {
+            System.out.print(stringArrays[i]);
+            System.out.print("-");
+        }
     }
 }
